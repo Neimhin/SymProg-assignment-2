5455e56bf46969db30953653aa8282b7156475c7
Neimhin <nrobinso@tcd.ie>
2020-12-05 16:46:14 +0000

    delete unused predicates

--- problem2.pl
+++ problem2.pl
@@ -1,6 +1,4 @@
-
-unique(A, B, C) :- nonvar(A), nonvar(B), nonvar(C), (A \= B), (B \= C), (C \= A).
-
+% Problem 2, Assignment 2
 unq_strt --> [h(Col1, Nat1, Pet1)], [h(Col2, Nat2, Pet2)], [h(Col3, Nat3, Pet3)],
               {
                 is_permutation([Col1, Col2, Col3], [red, blue, green]),
@@ -17,7 +15,6 @@ removeOrFail(H, [H|T], T).
 removeOrFail(H, [A|T] , [A|T_minus_H]) :-
           removeOrFail(H, T, T_minus_H).
           
-
 unique_street(
   street([  h([C1, N1, P1],[])
           , h([C2, N2, P2],[])
@@ -26,8 +23,6 @@ unique_street(
                   unique(N1,N2,N3),
                   unique(P1,P2,P3).
               
-street --> h, h, h. 
-
 h(C, N, P) :- colour(C), nationality(N),pet(P).
 
 colour(red).
